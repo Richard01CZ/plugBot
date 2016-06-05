902dfecaaed71836dc19d6e73597abe2f7c8ffd9
Richard01CZ <Richard.Budicek@email.cz>
2016-06-05 16:06:01 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -99,7 +99,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/bscBot/source/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/Richard01CZ/plugBot/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
