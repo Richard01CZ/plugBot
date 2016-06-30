19ee4672b24c6e8174e1039e2ff398e4dfe2fd84
Richard01CZ <Richard.Budicek@email.cz>
2016-06-30 15:03:47 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1,10 +1,3 @@
-/**
- *Copyright 2015 bscBot
- *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
- *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
- */
-
-
 (function () {
 
     /*window.onerror = function() {
@@ -299,7 +292,7 @@
             etaRestriction: false,
             welcome: true,
             opLink: null,
-            rulesLink: null,
+            rulesLink: "http://bit.ly/1TLonuq",
             themeLink: null,
             fbLink: null,
             youtubeLink: null,
